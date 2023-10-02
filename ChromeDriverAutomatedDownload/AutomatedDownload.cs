82c1c2297003f0aacc10e1f01c73fe1ec58fcf05
William Smith <williamsmithe@icloud.com>
2023-10-01 17:41:55 -0700

    Updated DownloadChromeDriverAsync() to accept two overloads.

--- ChromeDriverAutomatedDownload/AutomatedDownload.cs
+++ ChromeDriverAutomatedDownload/AutomatedDownload.cs
@@ -4,6 +4,11 @@ namespace ChromeForTestingAutomatedDownload
 {
     public static class AutomatedDownload
     {
+        public static async Task DownloadChromeDriverAsync(string downloadPath = "")
+        {
+            await DownloadChromeDriverAsync(MachineOSPlatform.GetPlatform(), downloadPath);
+        }
+
         public static async Task DownloadChromeDriverAsync(Platform platform, string downloadPath = "")
         {
             if (string.IsNullOrWhiteSpace(downloadPath)) downloadPath = AppDomain.CurrentDomain.BaseDirectory;
@@ -15,21 +20,21 @@ namespace ChromeForTestingAutomatedDownload
             var url = await model.GetMostRecentAssetURLByMajorReleaseNumberAsync(Binary.ChromeDriver, platform, localMajorRelease);
 
             using var httpClient = new HttpClient();
-            
+
             var response = await httpClient.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
             {
                 using var stream = await response.Content.ReadAsStreamAsync();
-                    
+
                 using var fileStream = File.Create(Path.Combine(downloadPath, Path.GetFileName(url) ?? string.Empty));
-                        
+
                 await stream.CopyToAsync(fileStream);
 
                 using ZipArchive archive = new ZipArchive(fileStream);
 
-                var driver = archive.Entries.Where(x => 
-                    Path.GetFileName(x.FullName).Equals("chromedriver.exe") || 
+                var driver = archive.Entries.Where(x =>
+                    Path.GetFileName(x.FullName).Equals("chromedriver.exe") ||
                     Path.GetFileName(x.FullName).Equals("chromedriver"))
                 .First();
 
---
+++ ChromeDriverAutomatedDownload/MachineOSPlatform.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ChromeForTestingAutomatedDownload
+{
+    public static class MachineOSPlatform
+    {
+        public static Platform GetPlatform()
+        {
+            string osDescription = RuntimeInformation.OSDescription;
+            Architecture processArchitecture = RuntimeInformation.ProcessArchitecture;
+
+            switch (osDescription)
+            {
+                case var windows when windows.Contains("Windows"):
+                    switch (processArchitecture)
+                    {
+                        case Architecture.X64:
+                            return Platform.Win64;
+                        case Architecture.X86:
+                            return Platform.Win32;
+                        default:
+                            throw new Exception("Unknown Windows architecture.");
+                    }
+
+                case var linux when linux.Contains("Linux"):
+                    switch (processArchitecture)
+                    {
+                        case Architecture.X64:
+                            return Platform.Linux64;
+                        default:
+                            throw new Exception("Unknown Linux architecture.");
+                    }
+
+                case var darwin when darwin.Contains("Darwin"):
+                    switch (processArchitecture)
+                    {
+                        case Architecture.X64:
+                            return Platform.MacX64;
+                        case Architecture.Arm64:
+                            return Platform.MacArm64;
+                        default:
+                            throw new Exception("Unknown macOS architecture.");
+                    }
+
+                default:
+                    throw new Exception("Unknown operating system.");
+            }
+        }
+    }
+}
