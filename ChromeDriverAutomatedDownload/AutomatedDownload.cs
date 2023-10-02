ae1a4f45c869e2ebf73b5ad1ba773aeeb03ae553
William Smith <williamsmithe@icloud.com>
2023-10-01 17:27:35 -0700

    Updated DownloadChromeDriverAsync method to accept multiple platforms passed by enum.

--- ChromeDriverAutomatedDownload/AutomatedDownload.cs
+++ ChromeDriverAutomatedDownload/AutomatedDownload.cs
@@ -4,7 +4,7 @@ namespace ChromeForTestingAutomatedDownload
 {
     public static class AutomatedDownload
     {
-        public static async Task DownloadChromeDriverAsync(string downloadPath = "")
+        public static async Task DownloadChromeDriverAsync(Platform platform, string downloadPath = "")
         {
             if (string.IsNullOrWhiteSpace(downloadPath)) downloadPath = AppDomain.CurrentDomain.BaseDirectory;
 
@@ -12,40 +12,33 @@ namespace ChromeForTestingAutomatedDownload
 
             var model = await ChromeVersionModelFactory.CreateChromeVersionModelAsync<LatestVersionsPerMilestoneWithDownload.ChromeVersionModel>();
 
-            var url = await model.GetMostRecentAssetURLByMajorReleaseNumberAsync(Binary.ChromeDriver, Platform.Win64, localMajorRelease);
+            var url = await model.GetMostRecentAssetURLByMajorReleaseNumberAsync(Binary.ChromeDriver, platform, localMajorRelease);
 
             using var httpClient = new HttpClient();
             
-            try
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await httpClient.GetAsync(url);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    using var stream = await response.Content.ReadAsStreamAsync();
+                using var stream = await response.Content.ReadAsStreamAsync();
                     
-                    using var fileStream = File.Create(Path.Combine(downloadPath, Path.GetFileName(url) ?? string.Empty));
+                using var fileStream = File.Create(Path.Combine(downloadPath, Path.GetFileName(url) ?? string.Empty));
                         
-                    await stream.CopyToAsync(fileStream);
+                await stream.CopyToAsync(fileStream);
 
-                    using ZipArchive archive = new ZipArchive(fileStream);
+                using ZipArchive archive = new ZipArchive(fileStream);
 
-                    var driver = archive.Entries.Where(x => x.FullName.Contains("chromedriver.exe")).First();
+                var driver = archive.Entries.Where(x => 
+                    Path.GetFileName(x.FullName).Equals("chromedriver.exe") || 
+                    Path.GetFileName(x.FullName).Equals("chromedriver"))
+                .First();
 
-                    await Task.Run(() => driver.ExtractToFile(Path.Combine(downloadPath, "chromedriver.exe"), true));
-
-                    Console.WriteLine("File downloaded successfully!");
-                }
-
-                else
-                {
-                    Console.WriteLine($"Failed to download file. Status code: {response.StatusCode}");
-                }
+                await Task.Run(() => driver.ExtractToFile(Path.Combine(downloadPath, Path.GetFileName(driver.FullName)), true));
             }
 
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                throw new Exception($"Failed to download file. Status code: {response.StatusCode}");
             }
         }
     }
